072000a4140f2363c5d34db8bd0a424aeef94fde
VoidKeishi <phananha4@gmail.com>
2025-05-03 17:22:55 +0700

    Refactor SDDM astronaut theme configuration: update embedded theme and streamline settings

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,29 +1,34 @@
 { config, pkgs, ... }:
+
 let
   custom-sddm-astronaut = pkgs.sddm-astronaut.override {
-    embeddedTheme = "japanese_aesthetic";
-    themeIni = {
-      General = {
-        background = "${pkgs.writeText "bg" ''/etc/sddm/nix-snowflake.png''}";
-        backgroundMode = "fill";
-        font = "Electroharmonix";
-      };
-    };
+    embeddedTheme = "hyprland_kath";
   };
-in {
-  environment.systemPackages = [
-    custom-sddm-astronaut
-    pkgs.kdePackages.qtsvg
-    pkgs.kdePackages.qtmultimedia
-    pkgs.kdePackages.qtvirtualkeyboard
-  ];
 
-  services.desktopManager.plasma6.enable = true;
+in {
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
-    theme = "astronaut";
-    extraPackages = [ custom-sddm-astronaut ];
-    settings = { Theme.Current = "astronaut"; };
+    wayland = {
+      enable = true;
+      compositor = "weston";
+    };
+    autoNumlock = true;
+    enableHidpi = true;
+    theme = "sddm-astronaut-theme";
+    settings = {
+      Theme = {
+        Current = "sddm-astronaut-theme";
+        CursorTheme = "Bibata-Modern-Ice";
+        CursorSize = 24;
+      };
+    };
+    extraPackages = with pkgs; [
+      custom-sddm-astronaut
+    ];
   };
+
+  environment.systemPackages = with pkgs; [
+    custom-sddm-astronaut
+    kdePackages.qtmultimedia
+  ];
 }
